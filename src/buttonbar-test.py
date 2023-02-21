96e66433e5bd9f366ae438e77d56ec8b0e83ac46
Bernhard Bablok <bablokb@gmx.de>
2023-02-21 19:38:51 +0100

    changed comment

--- src/buttonbar-test.py
+++ src/buttonbar-test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
-# A simple test for the button-bar
+# A simple test for the button-bar (Raspberry Pi version)
 #
 # Author: Bernhard Bablok
 # License: GPL3
